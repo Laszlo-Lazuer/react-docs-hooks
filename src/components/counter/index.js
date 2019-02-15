bfdc734aa779baedde3a784ac165f8b0167a712b
Laszlo Lazuer <visualjazz@gmail.com>
2019-02-15 13:36:44 -0600

    chore(counter) - adds counter example component.

---
+++ src/components/counter/index.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+export default function Counter() {
+    // Declare a new state variable, which we'll call "count"
+    const [count, setCount] = useState(0);
+
+    return(
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click Me
+            </button>
+            <button onClick={() => setCount(0)}>
+                Reset
+            </button>
+        </div>
+    )
+}
